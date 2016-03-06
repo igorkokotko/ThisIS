fe13e0409110de87b8ecf5e35107e62f66a3fa17
igorkokotko <thylacosmilu@gmail.com>
2016-03-06 14:08:18 +0000

    aded project

---
+++ site.js
@@ -0,0 +1,31 @@
+$('.part1').mouseover(function() {
+  $('.centered').css("visibility","visible");
+});
+
+$('.part1').mouseout(function() {
+  $('.centered').css("visibility","hidden");
+});
+
+$('.part2').mouseover(function() {
+  $('.next').css("visibility","visible");
+});
+
+$('.part2').mouseout(function() {
+  $('.next').css("visibility","hidden");
+});
+
+$('.part3').mouseover(function() {
+  $('.after').css("visibility","visible");
+});
+
+$('.part3').mouseout(function() {
+  $('.after').css("visibility","hidden");
+});
+
+$('.part4').mouseover(function() {
+  $('.fourth').css("visibility","visible");
+});
+
+$('.part4').mouseout(function() {
+  $('.fourth').css("visibility","hidden");
+});
