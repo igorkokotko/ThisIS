0d48e756eaad9016a62c254d7b226b486ea3840e
igorkokotko <thylacosmilu@gmail.com>
2016-03-07 11:21:51 +0000

    foot

--- site.js
+++ site.js
@@ -29,3 +29,30 @@ $('.part4').mouseover(function() {
 $('.part4').mouseout(function() {
   $('.fourth').css("visibility","hidden");
 });
+
+
+$(document).ready(function(){
+	$(function (){
+		$("#back-top").hide();
+	
+		$(window).scroll(function (){
+			if ($(this).scrollTop() > 100){
+				$("#back-top").fadeIn();
+			} else{
+				$("#back-top").fadeOut();
+			}
+		});
+		$("#back-top a").click(function (){
+			$("body,html").animate({
+				scrollTop:0
+			}, 800);
+			return false;
+		});
+	});
+});
+
+
+
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+})
